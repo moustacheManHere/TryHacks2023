5b7f40e5d9b276b89b591b8c7140f15e573cbbb9
Ryan <ryanyeojl@gmail.com>
2023-10-11 10:27:55 +0800

    Fix more stuff

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
 
-const Page = () => {
+import { useState, useEffect } from "react"
+// import Coins from './components/Coins'
+// import SearchCoins from './components/SearchCoins'
+
+export default function Home() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const getCoins = async () => {
+      const response = await fetch('/api/coins');
+      const coins = await response.json();
+      setCoins(coins.data.coins);
+    }
+
+    getCoins();
+  }, []);
+
+
   return (
-    <div>Page</div>
+//    <div className="text-center">
+//       <h1 className="font-bold text-6xl mt-14">Drug Infomation</h1>
+//       <SearchCoins getSearchResults={(results) => setCoins(results)} />
+//       <Coins coins={coins} />
+//    </div>
+<div>Page</div>
   )
 }
 
-export default Page
