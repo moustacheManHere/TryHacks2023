be41ab1318dcaefeca10a541207592dd600ea55c
SchwiDola-exe <83574147+SchwiDola-exe@users.noreply.github.com>
2023-10-11 09:53:00 +0800

    Update page.tsx

--- src/app/druginfo/page.tsx
+++ src/app/druginfo/page.tsx
@@ -1,7 +1,40 @@
+'use client'
+
+import Link from "next/link"
+import { FC, useState } from "react"
+
 const Home = () => {
     return (
         <h1>Welcome to my application!</h1>
     );
   };
-  
+  'use client'
+
+import Link from "next/link"
+import { useState } from "react"
+
+export default function searchapi({ getSearchResults }) {
+   const [query, setQuery] = useState('')
+
+   const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await fetch(`/api/coins/search?query=${query}`)
+
+    const api = await response.json()
+
+    getSearchResults(coin)
+
+   }
+
+  return (
+    <div className="text-center my-20">
+        <form onSubmit={handleSubmit}>
+            <input className="text-black border-2 border-black rounded-full px-3 py-2" type="text" placeholder="Search drug..." value={query} onChange={(e) => setQuery(e.target.value)} />
+            <button className="bg-black text-white rounded-full px-3 py-2 hover:bg-black/60" type="submit">Search</button>
+        </form>
+    </div>
+  )
+}
+
   export default Home;
